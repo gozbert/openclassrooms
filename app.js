443468d741b6ad8e3c14f2432290d30d0867112c
Gozbeth Stanslaus <gozbeths@gmail.com>
2020-02-03 09:35:44 +0300

    Adding async function for API calls

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
-function webPageLoaded(){
+function webPageLoaded() {
     alert('OK')
 }
 
 window.onload = webPageLoaded;
+
+
+var promise = new Promise(function (resolve, reject) {
+    // do a thing, possibly async, then…
+
+    if (/* everything turned out fine */) {
+        resolve("Stuff worked!");
+    }
+    else {
+        reject(Error("It broke"));
+    }
+});
+
+
+promise.then(function (result) {
+    console.log(result); // "Stuff worked!"
+}, function (err) {
+    console.log(err); // Error: "It broke"
+});
